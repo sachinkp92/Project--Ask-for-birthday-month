b0ecd7da591d51a07861b145b75c5315a1ba0755
Sachin Kumar <83978025+sachinkp92@users.noreply.github.com>
2021-05-13 16:49:52 +0530

    Update Project -Ask for birthday month.py

--- Project -Ask for birthday month.py
+++ Project -Ask for birthday month.py
@@ -1,4 +1,4 @@
-#Project- Ask for Birthday format DD-MM_YY, You were born in
+#Project- Ask for Birthday format DD-MM_YYYY, You were born in
 birthday=input("Enter your birthday in the format DD-MM-YYYY:")
 months=("January","Febuary","March","April","May","June","July","August","September","October","November","December")
 index=int(birthday[3:5])-1
